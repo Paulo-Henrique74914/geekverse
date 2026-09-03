466adb922fced9a7c32e0dec9fa8ebcd62a7ba89
Paulo-Henrique74914 <paulo.74914@aluno.pr.senac.br>
2026-09-03 11:16:30 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function enviarEmail() {
-  let email = document.querySelector("input[type='email']").value;
-
-  if (email === "") {
-    alert("Digite um email válido!");
-  } else {
-    alert("Email enviado com sucesso!");
-  }
-}
